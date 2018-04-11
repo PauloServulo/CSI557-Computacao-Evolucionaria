73f3f1b56dc5a0cee0183e04a26908ab54b94cb3
Fernando B Oliveira <fernando@Fernandos-MacBook-Pro.local>
2018-04-10 21:03:37 -0300

    Class Apr 10, 2018. R: update script; AG: TSP.

---
+++ Codes/AGTSP/src/agtsp/AGTSP.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agtsp;
+
+/**
+ *
+ * @author fernando
+ */
+public class AGTSP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Problema problema = new Problema("/Volumes/MacData/Dropbox/Dados/Desenvolvimento/Netbeans/CSI557-Computacao-Evolucionaria/Codes/instances/tsplib/berlin52.tsp");
+        
+        System.out.println(problema);
+
+        Integer tamanho = 50;
+        Double pCrossover = 0.8;
+        Double pMutacao = 0.05;
+        Integer geracoes = 100;
+        Integer nvar = problema.dimensao;
+        
+        AlgoritmoGenetico ag = new AlgoritmoGenetico(tamanho, pCrossover, pMutacao, geracoes, problema, nvar);
+
+        ag.executar();
+        
+    }
+    
+}
